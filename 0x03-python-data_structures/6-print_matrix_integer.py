8d811a815ddc20dc154d63c11fede9026119c212
Habeeb Afolabi Ojedokun <ojefolabi@yahoo.com>
2024-01-09 17:40:45 -0800

    update copy of the lists of lists.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for inte in row:
-            print("{:d}".format(inte), end="")
-        print()
+    if matrix:
+        for row in matrix:
+            for inte in row:
+                print("{:d}".format(inte), end=' ' if inte != row[-1] else '')
+            print()
