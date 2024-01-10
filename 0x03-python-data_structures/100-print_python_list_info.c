d3b045d1e22ef8102c206660e9f69c0e0284eb48
Habeeb Afolabi Ojedokun <ojefolabi@yahoo.com>
2024-01-09 20:53:23 -0800

    Leggo again

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -11,12 +11,21 @@
 
 void print_python_list_info(PyObject *p)
 {
-	long int size = PyList_Size(p);
-	int i;
-	PyListObject *obj = (PyListObject *)p;
+	if (!PyList_Check(p))
+	{
+		printf("[!] Error: Input is not a list.\n");
+		return;
+	}
 
-	printf("[*] Size of the Python List = %li\n", size);
-	printf("[*] Allocated = %li\n", obj->allocated);
-	for (i = 0; i < size; i++)
-		printf("Element %i: %s\n", i, PyTYPE (obj->ob_item[i])->tp_name);
+	Py_ssize_t list_size = PyList_Size(p);
+	Py_ssize_t allocated = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %zd\n", list_size);
+	printf("[*] Allocated = %zd\n", allocated);
+
+	for (Py_ssize_t i = 0; i < list_size; i++)
+	{
+		PyObject *element = PyList_GetItem(p, i);
+		printf("Element %zd: %s\n", i, Py_TYPE(element)->tp_name);
+	}
 }
