37ef1077e78ef63f3e8973870a7551e677d069ae
Habeeb Afolabi Ojedokun <ojefolabi@yahoo.com>
2024-01-15 16:17:07 -0800

    Trying the unique addition task again

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,6 @@
 #!/usr/bin?python3
 
 def uniq_add(my_list=[]):
-    unique_ints = set()
-    total_sum = 0
-
-    for num in my_list:
-        if num not in unique_ints:
-            unique_ints.add(num)
-            total_sum += num
-
+    unique_ints = set(my_list)
+    total_sum = sum(unique_ints)
     return (total_sum)
