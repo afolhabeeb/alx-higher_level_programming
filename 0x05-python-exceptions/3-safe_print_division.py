05a599ca3f6448ee59d88c67da7cddcc691839d4
Habeeb Afolabi Ojedokun <ojefolabi@yahoo.com>
2024-01-22 17:28:12 -0800

    Integer division with debug task.

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        div_result = a/b
+    except (ZeroDivisionError, TypeError):
+        div_result = None
+    finally:
+        print("Inside result: {}".format(div_result))
+    return(div_result)
