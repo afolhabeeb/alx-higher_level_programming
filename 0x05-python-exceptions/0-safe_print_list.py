046c3f98d55ada0c3abad6b31c423ed6ced79605
Habeeb Afolabi Ojedokun <ojefolabi@yahoo.com>
2024-01-22 16:55:20 -0800

    Safe printing of an integers list task.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for dig in range(x):
         try:
-            print("{}".format(my_list[dig]), end="")
+            print("{:d}".format(my_list[dig]), end="")
             count += 1
         except IndexError:
             break
