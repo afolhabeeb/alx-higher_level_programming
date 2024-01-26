6f7d2950c6413376775e86a14fd5e48e677a32b8
Habeeb Afolabi Ojedokun <ojefolabi@yahoo.com>
2024-01-26 01:00:24 -0800

    my first toolbox task: importing multiple function from a single file.

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-
 from calculator_1 import add, sub, mul, div
 
 a = 10
 b = 5
 
-add_result = add(a, b)
-print("{} + {} = {}".format(a, b, add_result))
-
-sub_result = sub(a, b)
-print("{} - {} = {}".format(a, b, sub_result))
-
-mul_result = mul(a, b)
-print("{} * {} = {}".format(a, b, mul_result))
-
-div_result = div(a, b)
-print("{} / {} = {}".format(a, b, div_result))
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
