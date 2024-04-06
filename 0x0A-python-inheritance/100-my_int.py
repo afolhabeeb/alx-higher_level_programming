8f158b0335573ffd897a590b4ed621f42e6f968b
Habeeb Afolabi Ojedokun <ojefolabi@yahoo.com>
2024-04-06 16:50:12 -0700

    Updated all the betty inscripted corrections.

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,7 +6,7 @@ class MyInt(int):
     """ My int inherits from int """
     def __eq__(self, num):
         """ Function for equals """
-        return(int(self) != int(num))
+        return (int(self) != int(num))
 
     def __ne__(self, num):
         """ Function for not equals """
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-""" New class """
-Rectangle = __import__("9-rectangle").Rectangle
+""" Exact same object """
 
 
-class Square(Rectangle):
-    """ Class square inherist from rectangle """
-    def __init__(self, size):
-        self.integer_validator("size", size)
-        self.__size = size
-        super().__init__(size, size)
+def is_same_class(obj, a_class):
+    """ Returns True if the object is exactly an instance
+    of the specified class """
+    return (type(obj) == a_class)
